999a6a09877de7cb48b5a24be10e04b73e5baf1f
juanitopons <juanpons1459@gmail.com>
2013-02-03 14:16:26 +0100

    Added GNU GPL to the project

--- src/com/jp/miaulavirtual/CurlService.java
+++ src/com/jp/miaulavirtual/CurlService.java
@@ -1,3 +1,22 @@
+/*
+* Copyright (C) 2013 Juan Pons (see README for details)
+* This file is part of miAulaVirtual.
+*
+* miAulaVirtual is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* miAulaVirtual is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with miAulaVirtual. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+*
+*/
+
 package com.jp.miaulavirtual;
 
 import java.io.BufferedInputStream;
